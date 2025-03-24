871973f5002b2a6cd7497581193881a2d2fcdaef
Merlin Thomas <emteeware@gmail.com>
2025-03-24 20:20:26 +0100

    Implement show handling

--- scaffolder.go
+++ scaffolder.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -54,8 +55,20 @@ func main() {
 			createStructure(basePath, contentType, mediaType, mediaProvider, name, year)
 
 		case "show":
-			fmt.Println("Shows not yet supported", line)
-			continue
+			contentType := "Serien"
+			if len(parts) != 7 {
+				fmt.Println("Invalid line format:", line)
+				continue
+			}
+			mediaType := parts[1]
+			mediaProvider := parts[2]
+			name := parts[3]
+			year := parts[4]
+			seasonNumber := parts[5]
+			episodeCount := parts[6]
+
+			createShowStructure(basePath, contentType, mediaType, mediaProvider, name, year, seasonNumber, episodeCount)
+
 		default:
 			fmt.Println("Invalid line format:", line)
 		}
@@ -63,6 +76,98 @@ func main() {
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
+	}
+}
+
+func createShowStructure(basePath string, contentType string, mediaType string, provider string, name string, year string, seasonNumber string, count string) {
+	err := os.Chdir(basePath)
+	if err != nil {
+		fmt.Println("Error changing directory:", err)
+		return
+	}
+
+	// Create contentType folder
+	err = os.MkdirAll(contentType, os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating folder:", err)
+		return
+	}
+
+	// Create show folder
+	err = os.Chdir(contentType)
+	if err != nil {
+		fmt.Println("Error changing directory:", err)
+		return
+	}
+	mediaFolderName := name + " (" + year + ")"
+	err = os.MkdirAll(mediaFolderName, os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating folder:", err)
+		return
+	}
+
+	// Create Season folder
+	err = os.Chdir(mediaFolderName)
+	if err != nil {
+		fmt.Println("Error changing directory:", err)
+		return
+	}
+	seasonFolderName := "Season " + fmt.Sprintf("%02s", seasonNumber)
+	err = os.MkdirAll(seasonFolderName, os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating folder:", err)
+		return
+	}
+
+	err = os.Chdir(seasonFolderName)
+	if err != nil {
+		fmt.Println("Error changing directory:", err)
+		return
+	}
+
+	infix := ""
+	fileType := ""
+	if mediaType == "disc" && provider == "bluray" {
+		infix = "BR Dummy"
+		fileType = "disc"
+	} else if mediaType == "disc" && provider == "dvd" {
+		infix = "DVD Dummy"
+		fileType = "disc"
+	} else if mediaType == "disc" && provider == "uhd" {
+		infix = "UHD Dummy"
+		fileType = "disc"
+	} else if mediaType == "stream" && provider == "apple" {
+		infix = "Apple TV Dummy"
+		fileType = "strm"
+	} else if mediaType == "stream" && provider == "amazon" {
+		infix = "Prime Video Dummy"
+		fileType = "strm"
+	} else {
+		fmt.Println("Unsupported mediaType: " + mediaType + " or provider: " + provider)
+		return
+	}
+
+	// iterate over episodes
+	episodes, err := strconv.Atoi(count)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	for i := 1; i <= episodes; i++ {
+		fileName := fmt.Sprintf("%s S%02sE%02d - %s.%s", mediaFolderName, seasonNumber, i, infix, fileType)
+		filePath := fileName
+		newFile, err := os.Create(filePath)
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+
+		// Write placeholder content to the file
+		_, err = newFile.WriteString(fmt.Sprintf("Content for %s", fileName))
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+		}
 	}
 }
 
